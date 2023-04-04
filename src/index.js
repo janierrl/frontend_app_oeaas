baaae67af1dbc22596c8c03aa51f591e06fd551e
Janier Rodríguez López <janierbits@yahoo.com>
2023-04-04 00:10:02 -0400

    fixed index

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-
-import reportWebVitals from './reportWebVitals';
 import ScreenRecording from './ScreenRecording';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
-  <ScreenRecording
-    screen={true}
-    audio={true}
-    video={true}
-    downloadRecordingPath="Captura"
-    downloadRecordingType=".mp4"
-    emailToSupport="support@xyz.com"
-  ></ScreenRecording>
-
+    <ScreenRecording
+      screen={true}
+      audio={true}
+      video={true}
+      downloadRecordingPath="Record"
+      downloadRecordingType="mp4"
+      emailToSupport="janierrl@yahoo.com"
+    ></ScreenRecording>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
